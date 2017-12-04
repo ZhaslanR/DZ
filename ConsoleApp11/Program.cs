ef50d63fa7a2e68a71be3372d75b09c1a06d7dc5
ZhaslanR <zhaslan_12@bk.ru>
2017-12-04 17:54:09 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstIndex, lastIndex;
+
+            RangeOfArray array = new RangeOfArray();
+
+            Console.WriteLine("Ведите нижнию границу массива: ");
+            if (!(int.TryParse(Console.ReadLine(),out firstIndex)))
+            {
+                Console.Clear();
+                Console.WriteLine("Ведите нижнию границу массива: ");
+            }
+            else
+            {
+                array.FirstIndex = firstIndex;
+            }
+
+            Console.WriteLine("Ведите верхнию границу массива: ");
+            if (!(int.TryParse(Console.ReadLine(), out lastIndex)))
+            {
+                Console.Clear();
+                Console.WriteLine("Ведите вехнию границу массива: ");
+            }
+            else
+            {
+                array.LastIndex = lastIndex;
+            }
+            array.Show();
+        }
+    }
+}
---
+++ ConsoleApp11/RangeOfArray.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    public class RangeOfArray
+    {
+        public int FirstIndex { get; set; }
+        public int LastIndex { get; set; }
+
+
+        private int[] mas;
+
+        public RangeOfArray()
+        {
+            mas = new int[LastIndex - FirstIndex]
+        }
+        //public int this[int index]
+        //{
+        //    get
+        //    {
+        //        if(FirstIndex > LastIndex)
+        //        {
+        //            if(index > FirstIndex && index < LastIndex)
+        //            {
+        //                return mas[index - LastIndex];
+        //            }
+        //            else
+        //            {
+        //                Console.WriteLine("Выход за границы массива");
+        //            }
+        //        }
+        //        else
+        //        {
+        //            if (index > FirstIndex && index < LastIndex)
+        //            {
+        //                return mas[index - FirstIndex];
+        //            }
+        //            else
+        //            {
+        //                Console.WriteLine("Выход за границы массива");
+        //            }
+        //        }
+        //    }
+        //    set
+        //    {
+        //        if (FirstIndex > LastIndex)
+        //        {
+        //            if (index > FirstIndex && index < LastIndex)
+        //            {
+        //                mas[index - LastIndex] = value;
+        //            }
+        //            else
+        //            {
+        //                Console.WriteLine("Выход за границы массива");
+        //            }
+        //        }
+        //        else
+        //        {
+        //            if (index > FirstIndex && index < LastIndex)
+        //            {
+        //                mas[index - FirstIndex] = value;
+        //            }
+        //            else
+        //            {
+        //                Console.WriteLine("Выход за границы массива");
+        //            }
+        //        }
+        //    }
+        //}
+
+        public void Show()
+        {
+            for(int i = FirstIndex;i<LastIndex;i++)
+            {
+                Console.Write("{0}\t", mas[i]);
+            }
+        }
+    }
+}
